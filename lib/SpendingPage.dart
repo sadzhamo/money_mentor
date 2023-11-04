d0992f33187bf0abda5b3f680a44bfe66b016b44
Jack Klebonis <jkleboni@asu.edu>
2023-11-04 13:29:04 -0700

    changed back

--- lib/SpendingPage.dart
+++ lib/SpendingPage.dart
@@ -12,7 +12,7 @@ class SpendingPage extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: ExpansionTile(
-        tilePadding: EdgeInsets.all(8.5),
+        tilePadding: EdgeInsets.all(8.0),
         trailing: Icon(
           Icons.arrow_drop_down,
           color: buttonText,
